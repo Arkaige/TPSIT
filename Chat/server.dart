4140ba4337c7957c18657e6372e29c9fdbc935bc
Arkaige <140431051+Arkaige@users.noreply.github.com>
2025-11-23 20:07:57 +0100

    Update server.dart

--- Chat/server.dart
+++ Chat/server.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() async {
   List<Socket> clients = [];
-  Map<Socket, String> clientNames = {}; // salva nome per socket
+  Map<Socket, String> clientNames = {}; // Salva nome dei client per socket
 
   final server = await ServerSocket.bind(InternetAddress.anyIPv4, 3000);
   print("Server executed on port 3000");
@@ -13,19 +13,17 @@ void main() async {
     // Primo messaggio dal client contiene il nome
     client.listen((data) {
       String msg = String.fromCharCodes(data).trim();
-
-      // Se non abbiamo ancora il nome, il primo messaggio è "Nome e' entrato nella chat"
+      
+      // Connessione di un client
       if (!clientNames.containsKey(client)) {
-        // estrai il nome dal messaggio (prima parola)
         String name = msg.split(" ")[0];
         clientNames[client] = name;
         print("CONNECTED ${client.remoteAddress.address}/$name");
       } else {
-        // Messaggi normali
         print(msg);
       }
 
-      // Inoltra agli altri client
+      // Manda il messaggio agli altri client
       for (var c in clients) {
         if (c != client) {
           c.write(msg + "\n");
@@ -34,11 +32,13 @@ void main() async {
     }, onDone: () {
       String? name = clientNames[client];
       print("DISCONNECTED ${client.remoteAddress.address}/$name");
+
       for (var c in clients) {
         if (c != client) {
           c.write("$name has left the chatroom\n");
         }
       }
+
       clients.remove(client);
       clientNames.remove(client);
     });

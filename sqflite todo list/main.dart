910ec2c9deb5982d876b5edbdc0d4cdf76934ea0
Arkaige <140431051+Arkaige@users.noreply.github.com>
2026-02-01 22:03:20 +0100

    Update main.dart

--- sqflite todo list/main.dart
+++ sqflite todo list/main.dart
@@ -1,6 +1,4 @@
-import 'helper.dart';
 import 'package:flutter/material.dart';
-
 import 'model.dart';
 import 'widgets.dart';
 
@@ -8,140 +6,119 @@ void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'am023 todo list floor',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-      ),
-      home: const MyHomePage(title: 'am023 todo list floor'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _textFieldController = TextEditingController();
-  final List<Todo> _todos = <Todo>[];
 
-  @override
-  initState() {
-    super.initState();
-    _init();
-  }
+class _HomeState extends State<Home> {
 
-  Future<void> _init() async {
-    await DatabaseHelper.init();
-    _updateTodos();
-  }
+  List<Note> notes = [];
 
-  void _updateTodos() {
-    DatabaseHelper.getTodos().then((todos) {
-      setState(() {
-        _todos.clear();
-        _todos.addAll(todos);
-      });
-    });
-  }
-
-  void _handleTodoChange(Todo todo) {
-    todo.checked = !todo.checked;
+  void addNote() {
     setState(() {
-      _todos.remove(todo);
-      if (!todo.checked) {
-        _todos.add(todo);
-      } else {
-        _todos.insert(0, todo);
-      }
+      notes.add(Note(todos: []));
     });
-    DatabaseHelper.updateTodo(todo);
   }
 
-  void _handleTodoDelete(Todo todo) {
+  void deleteNote(Note n) {
     setState(() {
-      _todos.remove(todo);
+      notes.remove(n);
     });
-    DatabaseHelper.deleteTodo(todo);
   }
 
-  Future<void> _displayDialog() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('add todo item'),
-          content: TextField(
-            controller: _textFieldController,
-            decoration: const InputDecoration(hintText: 'type here ...'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Add'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _addTodoItem(_textFieldController.text);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _addTodoItem(String name) {
-    Todo todo = Todo(id: null, name: name, checked: false);
+  void addTodo(Note n) {
     setState(() {
-      _todos.insert(0, todo);
+      n.todos.add(Todo(name: '', checked: false));
     });
-    DatabaseHelper.insertTodo(todo);
-    _textFieldController.clear();
+  }
+
+  void deleteTodo(Note n, Todo t) {
+    setState(() {
+      n.todos.remove(t);
+    });
+  }
+
+  void toggleTodo(Todo t) {
+    setState(() {
+      t.checked = !t.checked;
+    });
+  }
+
+  void changeText(Todo t, String txt) {
+    setState(() {
+      t.name = txt;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        shadowColor: Theme.of(context).shadowColor,
-        elevation: 4,
+        title: const Text('zKeep'),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: ListView.builder(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          itemCount: _todos.length,
-          itemBuilder: (context, index) {
-            return TodoItem(
-              todo: _todos[index],
-              onTodoChanged: _handleTodoChange,
-              onTodoDelete: _handleTodoDelete,
-            );
-          },
-        ),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, i) {
+          final note = notes[i];
+
+          return Card(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        deleteNote(note);
+                      },
+                    ),
+                  ],
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: note.todos.length,
+                  itemBuilder: (context, j) {
+                    return TodoItem(
+                      todo: note.todos[j],
+                      onDelete: (t) => deleteTodo(note, t),
+                      onCheck: toggleTodo,
+                      onChange: changeText,
+                    );
+                  },
+                ),
+                TextButton(
+                  onPressed: () => addTodo(note),
+                  child: const Text('aggiungi todo'),
+                ),
+              ],
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _displayDialog(),
-        tooltip: 'Add Item',
+        onPressed: addNote,
+        backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _textFieldController.dispose();
-    super.dispose();
   }
 }

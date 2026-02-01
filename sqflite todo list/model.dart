4e6f2bb312f217484ddac406de3c6be600957a2b
Arkaige <140431051+Arkaige@users.noreply.github.com>
2026-02-01 22:03:35 +0100

    Update model.dart

--- sqflite todo list/model.dart
+++ sqflite todo list/model.dart
@@ -1,15 +1,13 @@
 class Todo {
-  Todo({required this.id, required this.name, this.checked = false});
+  Todo({required this.name, required this.checked});
 
-  final int? id;
-  final String name;
+  String name;
   bool checked;
+}
 
-  Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name, 'checked': checked ? 1 : 0};
-  }
 
-  factory Todo.fromMap(Map<String, dynamic> map) {
-    return Todo(id: map['id'], name: map['name'], checked: map['checked'] == 1);
-  }
+class Note {
+  Note({required this.todos});
+
+  List<Todo> todos;
 }

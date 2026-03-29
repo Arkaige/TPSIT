c1dc0635fa3f3c7086554082d97f088b527255e4
Arkaige <140431051+Arkaige@users.noreply.github.com>
2026-03-29 20:08:18 +0200

    Update helper.dart

--- sqflite todo list/helper.dart
+++ sqflite todo list/helper.dart
@@ -1,69 +1,95 @@
-import 'model.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'model.dart';
 
 class DatabaseHelper {
   static Future<Database> init() async {
     // get path
-    String path = join(await getDatabasesPath(), 'todos.db');
+    String path = join(await getDatabasesPath(), 'zkeep.db');
 
     // open/create the database
-    return await openDatabase(path, version: 1, onCreate: _createTable);
+    return await openDatabase(path, version: 1, onCreate: _createTables);
   }
 
-  static Future<void> _createTable(Database db, int version) async {
-    // bool -> INTEGER
-    return await db.execute('''
-    CREATE TABLE todos (
-      id INTEGER NOT NULL PRIMARY KEY,
-      name TEXT NOT NULL,
-      checked INTEGER NOT NULL 
+  static Future<void> _createTables(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)'
     );
-    ''');
+    await db.execute(
+      'CREATE TABLE todos (id INTEGER PRIMARY KEY AUTOINCREMENT, noteId INTEGER, name TEXT, checked INTEGER)'
+    );
   }
 
-  static Future<List<Todo>> getTodos() async {
-    String path = join(await getDatabasesPath(), 'todos.db');
-    Database db = await openDatabase(path, version: 1);
-    /*
-    final List<Map<String, dynamic>> result = await db.rawQuery(
-       'SELECT * FROM todos',
-    );
-    */
-    final List<Map<String, dynamic>> result = await db.query('todos');
-    if (result.isEmpty) {
-      return <Todo>[];
+  
+//note
+  static Future<List<Note>> getNotes() async {
+    Database db = await init();
+    List<Map<String, dynamic>> righeNote = await db.query('notes');
+    List<Map<String, dynamic>> righeTodos = await db.query('todos');
+
+    List<Note> notes = [];
+    for (int i = 0; i < righeNote.length; i++) {
+      Note n = Note(righeNote[i]['title'], []);
+      n.id = righeNote[i]['id'];
+
+      for (int j = 0; j < righeTodos.length; j++) {
+        if (righeTodos[j]['noteId'] == n.id) {
+          Todo t = Todo(righeTodos[j]['name'], righeTodos[j]['checked'] == 1);
+          t.id = righeTodos[j]['id'];
+          t.noteId = n.id;
+          n.todos.add(t);
+        }
+      }
+
+      notes.add(n);
     }
-    // db.close();
-    return result.map((row) => Todo.fromMap(row)).toList();
+    return notes;
   }
 
-  static Future<void> insertTodo(Todo todo) async {
-    String path = join(await getDatabasesPath(), 'todos.db');
-    Database db = await openDatabase(path, version: 1);
-    // db.rawInsert('INSERT INTO todos(name, checked) values (?, ?)', [todo.name, todo.checked ? 1 : 0]);
-    db.insert('todos', todo.toMap());
-    // db.close();
+  static Future<int> insertNote() async {
+    Database db = await init();
+    Map<String, dynamic> mappa = {};
+    mappa['title'] = '';
+    int id = await db.insert('notes', mappa);
+    return id;
+  }
+
+  static Future<void> updateNote(Note n) async {
+    Database db = await init();
+    Map<String, dynamic> mappa = {};
+    mappa['title'] = n.title;
+    await db.update('notes', mappa, where: 'id = ?', whereArgs: [n.id]);
+  }
+
+  static Future<void> deleteNote(Note n) async {
+    Database db = await init();
+    await db.delete('todos', where: 'noteId = ?', whereArgs: [n.id]);
+    await db.delete('notes', where: 'id = ?', whereArgs: [n.id]);
+  }
+
+  
+//todo
+  static Future<int> insertTodo(Todo todo) async {
+    Database db = await init();
+    Map<String, dynamic> mappa = {};
+    mappa['noteId'] = todo.noteId;
+    mappa['name'] = todo.name;
+    mappa['checked'] = 0;
+    int id = await db.insert('todos', mappa);
+    return id;
   }
 
   static Future<void> updateTodo(Todo todo) async {
-    String path = join(await getDatabasesPath(), 'todos.db');
-    Database db = await openDatabase(path, version: 1);
-    // do not check existence!
-    // db.rawUpdate('UPDATE todos SET value = ? WHERE id = ?', [todo.checked ? 0 : 1, todo.id]);
-    db.update('todos', todo.toMap(), where: 'id = ?', whereArgs: [todo.id]);
-    // db.close();
+    Database db = await init();
+    Map<String, dynamic> mappa = {};
+    mappa['noteId'] = todo.noteId;
+    mappa['name'] = todo.name;
+    mappa['checked'] = todo.checked ? 1 : 0;
+    await db.update('todos', mappa, where: 'id = ?', whereArgs: [todo.id]);
   }
 
   static Future<void> deleteTodo(Todo todo) async {
-    String path = join(await getDatabasesPath(), 'todos.db');
-    Database db = await openDatabase(path, version: 1);
-    db.delete('todos', where: 'id = ?', whereArgs: [todo.id]);
-    // db.close();
-  }
-
-  Future<int> deleteAll() async {
-    String path = join(await getDatabasesPath(), 'todos.db');
-    Database db = await openDatabase(path, version: 1);
-    return await db.delete('todos');
+    Database db = await init();
+    await db.delete('todos', where: 'id = ?', whereArgs: [todo.id]);
   }
 }

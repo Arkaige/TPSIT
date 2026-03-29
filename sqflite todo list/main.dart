acfeeb69602d4fe8e1df2338438eb26fd0610f54
Arkaige <140431051+Arkaige@users.noreply.github.com>
2026-03-29 20:01:31 +0200

    Update main.dart

--- sqflite todo list/main.dart
+++ sqflite todo list/main.dart
@@ -1,113 +1,151 @@
 import 'package:flutter/material.dart';
 import 'model.dart';
 import 'widgets.dart';
+import 'helper.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
       home: Home(),
     );
   }
 }
 
-
 class Home extends StatefulWidget {
-  const Home({super.key});
-
   @override
   State<Home> createState() => _HomeState();
 }
 
-
 class _HomeState extends State<Home> {
-
   List<Note> notes = [];
 
-  void addNote() {
+  @override
+  void initState() {
+    super.initState();
+    loadNotes();
+  }
+
+  void loadNotes() async {
+    List<Note> list = await DatabaseHelper.getNotes();
     setState(() {
-      notes.add(Note(todos: []));
+      notes = list;
     });
   }
 
-  void deleteNote(Note n) {
+  void addNote() async {
+    Note n = Note('', []);
+    int id = await DatabaseHelper.insertNote();
+    n.id = id;
+    setState(() {
+      notes.add(n);
+    });
+  }
+
+  void deleteNote(Note n) async {
+    await DatabaseHelper.deleteNote(n);
     setState(() {
       notes.remove(n);
     });
   }
 
-  void addTodo(Note n) {
+  void addTodo(Note n) async {
+    Todo t = Todo('', false);
+    t.noteId = n.id;
+    int id = await DatabaseHelper.insertTodo(t);
+    t.id = id;
     setState(() {
-      n.todos.add(Todo(name: '', checked: false));
+      n.todos.add(t);
     });
   }
 
-  void deleteTodo(Note n, Todo t) {
+  void deleteTodo(Note n, Todo t) async {
+    await DatabaseHelper.deleteTodo(t);
     setState(() {
       n.todos.remove(t);
     });
   }
 
-  void toggleTodo(Todo t) {
+  void toggleTodo(Todo t) async {
     setState(() {
       t.checked = !t.checked;
     });
+    await DatabaseHelper.updateTodo(t);
   }
 
-  void changeText(Todo t, String txt) {
+  void changeText(Todo t, String txt) async {
     setState(() {
       t.name = txt;
     });
+    await DatabaseHelper.updateTodo(t);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('zKeep'),
+        title: Text('zKeep'),
         backgroundColor: Colors.green,
       ),
-      body: ListView.builder(
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
         itemCount: notes.length,
         itemBuilder: (context, i) {
-          final note = notes[i];
-
+          Note n = notes[i];
           return Card(
+            margin: EdgeInsets.all(8),
             child: Column(
               children: [
                 Row(
                   children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Titolo...',
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                        ),
+                        onChanged: (txt) {
+                          setState(() {
+                            n.title = txt;
+                          });
+                          DatabaseHelper.updateNote(n);
+                        },
+                      ),
+                    ),
                     IconButton(
-                      icon: const Icon(Icons.delete, color: Colors.red),
+                      icon: Icon(Icons.add, color: Colors.blue),
                       onPressed: () {
-                        deleteNote(note);
+                        addTodo(n);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        deleteNote(n);
                       },
                     ),
                   ],
                 ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: note.todos.length,
-                  itemBuilder: (context, j) {
-                    return TodoItem(
-                      todo: note.todos[j],
-                      onDelete: (t) => deleteTodo(note, t),
-                      onCheck: toggleTodo,
-                      onChange: changeText,
-                    );
-                  },
-                ),
-                TextButton(
-                  onPressed: () => addTodo(note),
-                  child: const Text('aggiungi todo'),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: n.todos.length,
+                    itemBuilder: (context, j) {
+                      return TodoItem(
+                        n.todos[j],
+                        (t) { deleteTodo(n, t); },
+                        toggleTodo,
+                        changeText,
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
@@ -115,9 +153,11 @@ class _HomeState extends State<Home> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: addNote,
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add),
+        onPressed: () {
+          addNote();
+        },
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
       ),
     );
   }
